582a16ccdfbd1b9bfbed6dcede2fa0991b22641d
unknown <mygarbage86@mail.ru>
2021-09-18 10:18:58 +0300

    fixed environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,14 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://cab-api',
+  baseUrl: 'http://api.ortum.injini.ru',
   /** Backend directory name (relatively @webroot) */
-  imagesDir: 'images'
+  imagesDir: 'images',
+  /** Relatively root */
+  licenses: [
+    {
+      path: 'assets/licenses/license.jpg',
+      image: 'assets/licenses/license.jpg',
+      thumb: 'assets/licenses/thumb.jpg'
+    }
+  ]
 };
